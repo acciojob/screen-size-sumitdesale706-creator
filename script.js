c0728174e4b87e245603ddaee91f08fae552a57e
Sumit Desale <sumitdesale706@gmail.com>
2026-09-22 00:42:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let div = document.createElement("div");
 div.id = "sizeInfo";
 
 let h1 = document.createElement("h1");
-
+h1.innerText = `Width ${window.innerWidth} and Height ${window.innerHeight}`;
 
 window.addEventListener("resize", () => {
 	h1.innerText = `Width ${window.innerWidth} and Height ${window.innerHeight}`;
