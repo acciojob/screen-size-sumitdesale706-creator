c31a347d4a0e88a435d1980704f7ae159a443064
Sumit Desale <sumitdesale706@gmail.com>
2026-09-22 00:33:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let body = document.querySelector("body");
+let display = document.createElement("h1");
+
+display.innerText = `Width ${window.innerWidth} and Height ${window.innerHeight}`;
+
+body.appendChild(display);
